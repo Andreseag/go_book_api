8a61f8d1588d3c79a57b825e624fbf5a79e2bd77
Edwin Castro <edwin.castro@agenciamatch.cl>
2025-11-02 21:20:14 -0500

    chore: init project :rocket:

---
+++ cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"go_book_api/api"
+)
+
+func main() {
+	api.InitDB()
+	r := gin.Default()
+
+	//routes
+	r.POST("/book", api.CreateBook)
+	r.GET("/books", api.GetBooks)
+	r.GET("/book/:id", api.GetBook)
+	r.PUT("/book/:id", api.UpdateBook)
+	r.DELETE("/book/:id", api.DeleteBook)
+
+	r.Run(":8080")
+}
