2427f44854e83a20f1b34c6e8cc208855a5a4141
Edwin Castro <edwin.castro@agenciamatch.cl>
2025-11-02 21:54:00 -0500

    feat: add auth token validation :sparkles:

--- api/handlers.go
+++ api/handlers.go
@@ -8,9 +8,41 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"github.com/golang-jwt/jwt/v5"
+	"time"
 )
 
 var DB *gorm.DB
+
+
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func GenerateJWT(c *gin.Context) {
+	var loginRequest LoginRequest
+	if err := c.ShouldBindJSON(&loginRequest); err != nil {
+		ResponseJSON(c, http.StatusBadRequest, "Invalid request payload", nil)
+		return
+	}
+	if loginRequest.Username != "admin" || loginRequest.Password != "password" {
+		ResponseJSON(c, http.StatusUnauthorized, "Invalid credentials", nil)
+		return
+	}
+	expirationTime := time.Now().Add(15 * time.Minute)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"exp": expirationTime.Unix(),
+	})
+	// Sign the token
+	tokenString, err := token.SignedString(jwtSecret)
+	if err != nil {
+		ResponseJSON(c, http.StatusInternalServerError, "Could not generate token", nil)
+		return
+	}
+	ResponseJSON(c, http.StatusOK, "Token generated successfully", gin.H{"token": tokenString})
+}
+
 
 func InitDB() {
 	err := godotenv.Load()
